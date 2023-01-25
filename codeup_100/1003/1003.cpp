7a9e25d679542bf5c55008c0b09521af0e649c03
ralph <hwibaski@gmail.com>
2023-01-25 22:48:33 +0900

    2023-01-25: codeup_100

---
+++ codeup_100/1003/1003.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello" << '\n';
+	cout << "World" << '\n';
+	return 0;
+}
---
+++ codeup_100/1015/1015.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	float f;
+	cin >> f;
+	cout << fixed << setprecision(2) << f;
+
+	return 0;
+}
